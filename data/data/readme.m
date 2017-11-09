6a5ab995f32f138704a2a4412ddf86a944283a5d
yihongXU <hongxu520@gmail.com>
2017-11-09 23:56:06 +0100

    data for training, validation and test

---
+++ data/data/readme.m
@@ -0,0 +1,2 @@
+"small data" folder for data from kaggle dataset https://www.kaggle.com/c/dog-breed-identification/data
+"data" folder for data from stanford dataset http://vision.stanford.edu/aditya86/ImageNetDogs/
--- data/readme.m
+++ data/readme.m
@@ -1,1 +1,9 @@
-this is a folder for data from https://www.kaggle.com/c/dog-breed-identification/data
+"small_dataset" is a folder for kaggle dataset from https://www.kaggle.com/c/dog-breed-identification/data
+"data" is a well seperated dataset (including labels information) for images from stanford dataset http://vision.stanford.edu/aditya86/ImageNetDogs/
+
+"res_incep" contains bottlenecks exctract from Resnet_Inception v2 for validation and training
+
+"test_res_incep" contains bottlenecks exctract from Resnet_Inception v2 for testing
+
+"models" folder contains trained fully connected layer models
+(resnet_Inception get the best result for now)
